65513c26f42477c917a0c8e1b804b38d6ae3169a
Noor A Alam <noorsomrat2000@gmail.com>
2025-01-13 18:14:15 +0600

    Database Connected

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 var cors = require('cors')
+var mysql = require('mysql2');
+var db = mysql.createConnection({
+host: "localhost",
+user: "root",
+password: "",
+database: "noor_nourishment"
+});
 //middleware
 app.use(cors())
 app.use(express.json())
